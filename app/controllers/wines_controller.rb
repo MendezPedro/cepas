973f6238cdfe158dd228fef59d956eade960fe57
Pedro <mendez.pedro94@gmail.com>
2021-11-15 22:28:24 -0300

    test antes de eliminar la carpeta test para usar la gema

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,5 +1,6 @@
 class WinesController < ApplicationController
   before_action :set_wine, only: %i[ show edit update destroy ]
+  skip_before_action :authenticate_user!, only: [:index]
   skip_before_action :only_admin, only: [:index]
   # GET /wines or /wines.json
   def index
--- test/controllers/wines_controller_test.rb
+++ test/controllers/wines_controller_test.rb
@@ -5,44 +5,13 @@ class WinesControllerTest < ActionDispatch::IntegrationTest
     @wine = wines(:one)
   end
 
-  test "should get index" do
-    get wines_url
+  test 'get index' do
+    get '/wines'
     assert_response :success
   end
 
-  test "should get new" do
-    get new_wine_url
+  test 'get wine' do
+    get '/wines' + @wine.id.to_s
     assert_response :success
-  end
-
-  test "should create wine" do
-    assert_difference('Wine.count') do
-      post wines_url, params: { wine: { name: @wine.name } }
-    end
-
-    assert_redirected_to wine_url(Wine.last)
-  end
-
-  test "should show wine" do
-    get wine_url(@wine)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_wine_url(@wine)
-    assert_response :success
-  end
-
-  test "should update wine" do
-    patch wine_url(@wine), params: { wine: { name: @wine.name } }
-    assert_redirected_to wine_url(@wine)
-  end
-
-  test "should destroy wine" do
-    assert_difference('Wine.count', -1) do
-      delete wine_url(@wine)
-    end
-
-    assert_redirected_to wines_url
   end
 end
